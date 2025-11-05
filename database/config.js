0f9a7f46d465b0f720fac63d4c52c1fbbc6d188d
dryzxmodders <dryzxmodders@gmail.com>
2025-11-05 15:51:38 +0700

    Add database/config.js

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8371501056:AAF3Af42cBFsPHaE3EZXYYvtspiH3j1JmxE",  // Ubah Jadi Token Bot Mu !!!
+  owner: "8157933109", // Ubah Jadi Id Mu !!!
+  port: "1466", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "-" // Ubah Jadi Ip Vps Mu !!!
+};
